838fdfe7667c8d25bd61cb5588e9e0cedfe88d1b
Oren Koriat <oren@cynerio.co>
2021-04-17 18:59:03 +0300

    add tree constructors

--- src/trees_and_graphs.rs
+++ src/trees_and_graphs.rs
@@ -10,26 +10,42 @@ struct Tree<T> {
 }
 
 
+impl<T> Node<T> {
+    pub fn new(data: T, children: Vec<Node<T>>) -> Node<T> {
+        Node {
+            data,
+            children
+        }
+    }
+}
+
+impl<T> Tree<T> {
+    pub fn new(root: Node<T>) -> Tree<T> {
+        Tree {
+            root
+        }
+    }
+}
+
 #[cfg(test)]
 mod tests {
     use crate::trees_and_graphs::{Node, Tree};
 
     #[test]
-    fn new_test() {
-        let tree = Tree {
-            root: Node {
-                data: 3,
-                children: vec![Node{data: 2, children: vec![]}, Node{data: 0, children: vec![]}]
-            }
-        };
+    fn test_tree_construction() {
+        let root = Node::new(
+            3,
+            vec![Node::new(2, vec![]), Node::new(0, vec![])]
+        );
 
-        let str_tree = Tree {
-            root: Node {
-                data: "str",
-                children: vec![Node{data: "inner str", children: vec![]}]
-            }
-        };
+        let tree = Tree::new(root);
 
+        let root = Node::new("str",
+                            vec![Node::new("inner str", vec![])]);
+
+        let str_tree = Tree::new(root);
+
+        assert_eq!(3, tree.root.data);
         assert_eq!(2, tree.root.children[0].data);
         assert_eq!(0, tree.root.children[1].data);
 
