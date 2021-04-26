e8eb6815567278454f1509ecfaf87b1db272dde6
Oren Koriat <oren@cynerio.co>
2021-04-26 22:27:12 +0300

    BFS scrappy implementation

--- src/graphs.rs
+++ src/graphs.rs
@@ -5,7 +5,6 @@ use std::collections::{HashMap, HashSet};
 use petgraph::visit::FilterNode;
 use petgraph::data::Element::Node;
 use queues::*;
-// use std::hash::Hash;
 
 fn handle_neighbors(queue: & mut Queue<NodeIndex>, visited: &HashSet<NodeIndex>,
                             neighbors: Neighbors<u32, u32>) {
@@ -20,7 +19,17 @@ fn handle_neighbors(queue: & mut Queue<NodeIndex>, visited: &HashSet<NodeIndex>,
 fn search_path(graph: Graph<&str, u32>, from: NodeIndex, to: NodeIndex) {
     let mut q = queue![];
     let mut visited: HashSet<petgraph::prelude::NodeIndex> = HashSet::new();
-    handle_neighbors(&mut q, &visited, graph.neighbors_directed(from, Direction::Outgoing))
+    handle_neighbors(&mut q, &visited, graph.neighbors_directed(from, Direction::Outgoing));
+
+    while q.size() > 0 {
+        let node = q.remove().expect("expected queue to contain a NodeIndex");
+        handle_neighbors(&mut q, &visited, graph.neighbors_directed(node, Direction::Outgoing));
+        if node == to {
+            println!("Found path! {:?}, visited: {:?}", graph[node], &visited);
+        }
+        visited.insert(node);
+
+    }
 }
 
 #[cfg(test)]
@@ -33,11 +42,15 @@ mod tests {
         let givat_zeev = g.add_node("givat_zeev");
         let jerusalem = g.add_node("jerusalem");
         let tel_aviv = g.add_node("tel_aviv");
+        let ramat_gan = g.add_node("ramat gan");
+        let yahud = g.add_node("yahud");
         let givat_shmuel = g.add_node("givat_shmuel");
         g.add_edge(givat_zeev, jerusalem, 0);
         g.add_edge(givat_zeev, tel_aviv, 0);
         g.add_edge(jerusalem, tel_aviv, 0);
-        g.add_edge(tel_aviv, givat_shmuel, 0);
+        g.add_edge(tel_aviv, yahud, 0);
+        g.add_edge(tel_aviv, ramat_gan, 0);
+        g.add_edge(ramat_gan, givat_shmuel, 0);
         search_path(g, givat_zeev, givat_shmuel);
     }
 }
