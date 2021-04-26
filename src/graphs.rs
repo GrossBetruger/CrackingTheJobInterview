9b3b9f41f2c7436bfb4f1568182b8dc63a78412b
Oren Koriat <oren@cynerio.co>
2021-04-26 22:06:53 +0300

    extract: handle neighbors

--- src/graphs.rs
+++ src/graphs.rs
@@ -1,25 +1,34 @@
-use petgraph::graph::{NodeIndex, Graph};
+use petgraph::graph::{NodeIndex, Graph, Neighbors};
 use petgraph::Direction;
+use queues::{Queue, IsQueue};
+use std::collections::{HashMap, HashSet};
+use petgraph::visit::FilterNode;
+use petgraph::data::Element::Node;
+use queues::*;
+// use std::hash::Hash;
 
-
-fn search_path(graph: Graph<&str, u32>, from: NodeIndex, to: NodeIndex) {
-    for neighbor in graph.neighbors_directed(from, Direction::Outgoing)
-        .into_iter() {
-        println!("{}", graph[neighbor]);
+fn handle_neighbors(queue: & mut Queue<NodeIndex>, visited: &HashSet<NodeIndex>,
+                            neighbors: Neighbors<u32, u32>) {
+    for neighbor in neighbors.into_iter() {
+        if visited.contains(&neighbor) {
+            continue
+        }
+        queue.add(neighbor.clone());
     }
 }
+
+fn search_path(graph: Graph<&str, u32>, from: NodeIndex, to: NodeIndex) {
+    let mut q = queue![];
+    let mut visited: HashSet<petgraph::prelude::NodeIndex> = HashSet::new();
+    handle_neighbors(&mut q, &visited, graph.neighbors_directed(from, Direction::Outgoing))
+}
+
 #[cfg(test)]
 mod tests {
-    use petgraph::graph::{NodeIndex, Graph};
-    use petgraph::visit::Bfs;
-    use queues::*;
-    use petgraph::Direction;
-    use petgraph::visit::Visitable;
-    use crate::graphs::search_path;
-
+    use super::*;
     #[test]
     fn graph() {
-        let mut q: Queue<isize> = queue![];
+
         let mut g: Graph<&str, u32> = Graph::new();
         let givat_zeev = g.add_node("givat_zeev");
         let jerusalem = g.add_node("jerusalem");
@@ -29,7 +38,6 @@ mod tests {
         g.add_edge(givat_zeev, tel_aviv, 0);
         g.add_edge(jerusalem, tel_aviv, 0);
         g.add_edge(tel_aviv, givat_shmuel, 0);
-
         search_path(g, givat_zeev, givat_shmuel);
     }
 }
